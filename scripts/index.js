4730ce90aeb20624559c2268e89059ccda884c3a
Peter Potapov <dr.potapoff-peter@yandex.ru>
2022-07-31 12:48:16 +0300

    fix: исправить код в функциях

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__pen");
 let closeButton = document.querySelector(".popup__button-drop");
-let formTitle = document.querySelector(".popup__info-title");
-let formSubtitle = document.querySelector(".popup__info-subtitle");
+let formTitle = document.querySelector(".profile__title");
+let formSubtitle = document.querySelector(".profile__subtitle");
 let saveButton = document.querySelector(".popup__container");
 
 function openPopup() {
@@ -29,12 +29,14 @@ editButton.addEventListener("click", editHandler);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameInput = formTitle.value;
-  jobInput = formSubtitle.value;
+  nameInput = document.querySelector("popup__info-title").value;
+  jobInput = document.querySelector("popup__info-subtitle").value;
+  // nameInput = formTitle.value;
+  // jobInput = formSubtitle.value;
   formTitle.textContent = nameInput;
   formSubtitle.textContent = jobInput;
 
-  closePopup();
+  // closePopup();
 }
 
 saveButton.addEventListener("submit", formSubmitHandler);

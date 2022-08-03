c7f571888da946977d0b637d59219ff56bf584aa
Peter Potapov <dr.potapoff-peter@yandex.ru>
2022-08-03 17:29:00 +0300

    fix: исправить все CSS после 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__pencil");
 let closeButton = document.querySelector(".popup__button-drop");
-let formTitle = document.querySelector(".profile__title");
-let formSubtitle = document.querySelector(".profile__subtitle");
-let popupForm = document.querySelector("#popup__form");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+let popupForm = document.querySelector(".popup__form");
 let nameInput;
 let jobInput;
 
 function openPopup() {
   popup.classList.add("popup_opened");
   document.getElementById("popup__info_form_title").value =
-    formTitle.textContent;
+    profileTitle.textContent;
   document.getElementById("popup__info_form_subtitle").value =
-    formSubtitle.textContent;
+    profileSubtitle.textContent;
 }
 
 function closePopup() {
@@ -23,8 +23,8 @@ closeButton.addEventListener("click", closePopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameInput = document.querySelector("popup__info_form_title").value;
-  jobInput = document.querySelector("popup__info_form_subtitle").value;
+  nameInput = document.querySelector(".popup__info_form_title").value;
+  jobInput = document.querySelector(".popup__info_form_subtitle").value;
   formTitle.textContent = nameInput;
   formSubtitle.textContent = jobInput;
 
